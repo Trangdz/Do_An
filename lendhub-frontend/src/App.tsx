849132d2bbddb8468a6c28441dd696b4683fc0d8
Mr.Trang <trinhvantrangok12321@gmail.com>
2025-09-14 10:44:06 +0700

    Chart_pr4__p2

--- lendhub-frontend/src/App.tsx
+++ lendhub-frontend/src/App.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import { ModernDashboard } from './components/ModernDashboard';
+import { SimpleDashboard } from './components/SimpleDashboard';
 
 function App() {
-  return <ModernDashboard />;
+  return <SimpleDashboard />;
 }
 
 export default App;
