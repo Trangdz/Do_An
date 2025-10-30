877958273b42d7c643833f3c65a246aa0921d743
Mr.Trang <trinhvantrangok12321@gmail.com>
2025-10-30 19:55:27 +0700

    Buid chainlink

--- lendhub-frontend-nextjs/.next/server/middleware-build-manifest.js
+++ lendhub-frontend-nextjs/.next/server/middleware-build-manifest.js
@@ -28,11 +28,6 @@ globalThis.__BUILD_MANIFEST = {
       "static/chunks/main.js",
       "static/chunks/pages/borrow/[symbol].js"
     ],
-    "/history": [
-      "static/chunks/webpack.js",
-      "static/chunks/main.js",
-      "static/chunks/pages/history.js"
-    ],
     "/liquidations": [
       "static/chunks/webpack.js",
       "static/chunks/main.js",
